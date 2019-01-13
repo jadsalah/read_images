c8b932126ef621894954387da27a36c5bb6d15a2
jadsalah <jadsalah2@gmail.com>
2019-01-13 12:03:37 +0100

    Update read_centroids.py

--- read_images/src/read_centroids.py
+++ read_images/src/read_centroids.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python
-
+"""
+This node will read values from the centroids topic and send them to cmd_vel topic as commands
+The Control is just A P control, but i think we will need to add the saturated I and The D controls to the real Drone
+"""
 import rospy
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
 import sys
+from std_msgs.msg import String
+import numpy as np
+from matplotlib import pyplot as plt
+import select as slt
 
 class send_cmd:
 
 	def __init__(self):
 		self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-		self.subscriber = rospy.Subscriber("/centroids",Float32, self.callback)
+		self.subscriber = rospy.Subscriber("/centroids",String, self.callback)
+		self.centroids = np.empty((0,1), int)
 
 	def mycommand(self,dat):
 		self.pub.publish(dat)
 
+	def saveData(self):
+		np.savetxt("/home/jad/catkin_ws/src/read_images/src/dat.csv", self.centroids, delimiter=",")
+
 	def callback(self,ros_data):
+		rec_msg = ros_data.data
+		r_cx = float(rec_msg.split("_")[0])
+		r_sD = float(rec_msg.split("_")[1])
+		self.centroids = np.append(self.centroids,r_cx)
 		vel_msg = Twist()
-		r_cx = ros_data.data
 		Kp = 0.0005
+		Kp2 = 0.2
 		err = 320 - r_cx
-		vel_msg.linear.x = 2
-		vel_msg.linear.y = 0
+		vel_msg.linear.x = 1
+		vel_msg.linear.y = Kp2*r_sD
 		vel_msg.linear.z = 0
 		vel_msg.angular.x = 0
 		vel_msg.angular.y = 0
@@ -32,11 +47,8 @@ class send_cmd:
 def main(args):
 	sc = send_cmd()
 	rospy.init_node('send_command', anonymous=True)
-	try:
-		rospy.spin()
-	except KeyboardInterrupt:
-		print "Shutting down ROS Image feature detector module"
-		
+	rospy.spin()
+	sc.saveData()
 if __name__ == '__main__':
 	main(sys.argv)
 
