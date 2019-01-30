1a15a0728ff4d0cc2db168ec14299956a834adf2
jadsalah <jadsalah2@gmail.com>
2019-01-30 15:16:28 +0100

    Update read_centroids.py

--- read_images/src/read_centroids.py
+++ read_images/src/read_centroids.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-"""
-This node will read values from the centroids topic and send them to cmd_vel topic as commands
-The Control is just A P control, but i think we will need to add the saturated I and The D controls to the real Drone
-"""
+
 import rospy
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
@@ -18,6 +15,8 @@ class send_cmd:
 		self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 		self.subscriber = rospy.Subscriber("/centroids",String, self.callback)
 		self.centroids = np.empty((0,1), int)
+		self.newr_cx = 0
+		self.oldr_cx = 0
 
 	def mycommand(self,dat):
 		self.pub.publish(dat)
@@ -29,19 +28,25 @@ class send_cmd:
 		rec_msg = ros_data.data
 		r_cx = float(rec_msg.split("_")[0])
 		r_sD = float(rec_msg.split("_")[1])
-		self.centroids = np.append(self.centroids,r_cx)
+		
+		self.newr_cx = r_cx
+		if self.oldr_cx != 0:
+			self.newr_cx = self.oldr_cx + 0.15*(self.newr_cx-self.oldr_cx)
+		self.centroids = np.append(self.centroids,self.newr_cx)
 		vel_msg = Twist()
 		Kp = 0.0005
-		Kp2 = 0.2
-		err = 320 - r_cx
-		vel_msg.linear.x = 1
+		Kp2 = 0.02
+		err = 320 - self.newr_cx
+		vel_msg.linear.x = 0.5
 		vel_msg.linear.y = Kp2*r_sD
+		#vel_msg.linear.y = 0
 		vel_msg.linear.z = 0
 		vel_msg.angular.x = 0
 		vel_msg.angular.y = 0
 		vel_msg.angular.z = Kp*err
 		self.mycommand(vel_msg)
 		print str(ros_data.data)
+		self.oldr_cx = self.newr_cx 
 
 
 def main(args):
